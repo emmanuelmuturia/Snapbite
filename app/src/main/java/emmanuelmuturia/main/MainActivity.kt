293001258bb8a21f4fa7fc5a27a1d11829d79707
Emmanuel Muturia <science@emmanuelmuturia.com>
2023-10-23 13:32:05 +0300

    Setting up the Feature Modules and Modifying the Home Screen...

--- app/src/main/java/emmanuelmuturia/main/MainActivity.kt
+++ app/src/main/java/emmanuelmuturia/main/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import emmanuelmuturia.navgraph.NavGraph
@@ -15,6 +16,7 @@ import emmanuelmuturia.theme.SnapbiteTheme
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContent {
             SnapbiteTheme {
@@ -23,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    NavGraph(navController = rememberNavController())
+                    emmanuelmuturia.navgraph.NavGraph(navController = rememberNavController())
                 }
             }
         }
